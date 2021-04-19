7d3add83a28112bd0abb2d8d7bdf6391dd721912
Mykhailo Sorochynskyi <mishash20161506@gmail.com>
2021-04-19 21:46:47 +0300

    added mdbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
